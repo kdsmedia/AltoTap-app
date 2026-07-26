11ea148104767b44c0781cd4a6282deaee868234
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2026-07-26 01:24:01 +0000

    Upload

--- artifacts/mobile/app/_layout.tsx
+++ artifacts/mobile/app/_layout.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { ImageBackground, StyleSheet } from 'react-native';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { KeyboardProvider } from 'react-native-keyboard-controller';
